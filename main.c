207db6cf82d1157d89d263a65e01f1870988ca5d
Sam Meech-Ward <sam@meech-ward.com>
2017-07-24 12:00:15 -0700

    Add the start of a fizz buzz algorithm

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
-    
+    for (int i = 0; i <= 100; ++i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("Fizz Buzz\n");
+        }
+    }
     return 0;
 }
 
